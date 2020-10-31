fa7600e340af6a72cccadd4e12a117c6fb0462d9
voronindim <51754697+voronindim@users.noreply.github.com>
2020-10-31 21:04:33 +0300

    Добавил storyboard

---
+++ lab_9/FigureWithStoryboard/FigureWithStoryboard/Shapes.swift
@@ -0,0 +1,8 @@
+//
+//  Shapes.swift
+//  FigureWithStoryboard
+//
+//  Created by Dmitrii Voronin on 31.10.2020.
+//
+
+import Foundation
---
+++ lab_9/FigureWithStoryboard/FigureWithStoryboard/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  FigureWithStoryboard
+//
+//  Created by Dmitrii Voronin on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var canvas: UIView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: @IBAction Commands
+    
+    @IBAction
+    func save(_ sender: Any) {
+    }
+    
+    @IBAction
+    func open(_ sender: Any) {
+    }
+    
+    @IBAction
+    func redo(_ sender: Any) {
+    }
+    
+    @IBAction
+    func undo(_ sender: Any) {
+    }
+    
+    @IBAction
+    func what(_ sender: Any) {
+    }
+    
+    // MARK: @IBAction CreateFigure
+    
+    @IBAction
+    func createElipse(_ sender: Any) {
+    }
+    
+    @IBAction
+    func createTringle(_ sender: Any) {
+    }
+    
+    @IBAction
+    func createRectangle(_ sender: Any) {
+    }
+    
+}
+
